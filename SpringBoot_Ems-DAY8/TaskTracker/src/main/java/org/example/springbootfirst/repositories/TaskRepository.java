795c5b1ca195a49587fabea9167a964a37e8c18e
MohamedAnas3202 <mohamedanas.a2023cse@sece.ac.in>
2025-07-15 10:07:16 +0530

    Day-8 TASK updated

--- SpringBoot_Ems-DAY8/TaskTracker/src/main/java/org/example/springbootfirst/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.springbootfirst.repositories;
-
-import org.example.springbootfirst.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-    List<Task> findByStatus(String status);
-}
